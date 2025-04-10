136136aee7d993365562804b7afc3d4b58852025
twenty2industries <burak.bostanli.1@web.de>
2025-04-10 22:52:42 +0200

    comments are rendering now

---
+++ template.js
@@ -0,0 +1,25 @@
+/* 
+function renderComment(commentsIndex){
+  const commentsRef = document.getElementById('comments-section');
+  for (let commentIndex = 0; commentIndex < books[commentsIndex].comments.length; commentIndex++) {
+    const element = books[commentsIndex].comments[commentIndex].comment;  
+    commentsRef.innerHTML += renderComments();
+  }
+}
+function returnComments(indexPosition){
+  return `<div id="comments-section-${indexPosition}" class="commentsSection">
+    <div id="show-username-${indexPosition}" class="showUsername">${books[indexPosition].comments.name}</div>
+    <div id="show-comments-${indexPosition}" class="showComments"></div>
+  </div>
+  <input type="text" />KOMMENTAR INPUT`;
+}
+ */
+
+/*     renderComments(bookIndex);
+    returnComment(bookIndex); */
+
+
+/* function returnImg(){
+  const imgRef = document.getElementById('')
+  return `<img src="${books[i].img}">`
+} */
