c4eabfcb11c1b7f2381d32af32b3e0d72b7321fa
twenty2industries <burak.bostanli.1@web.de>
2025-04-13 20:50:19 +0200

    fixPrice()succesfully implemented

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function init() {
 
 function renderContent(newCommentIndex) {
   const contentRef = document.getElementById("content-area");
-  const commentRef = document.getElementById(`comment-area-${newCommentIndex}`);
   for (let bookIndex = 0; bookIndex < books.length; bookIndex++) {
     contentRef.innerHTML += returnContent(bookIndex);
     renderComment(bookIndex);
     toggleLike(bookIndex);
-  }
+    fixPrice(bookIndex);
+}
 }
 
 function renderComment(commentsIndex) {
@@ -40,7 +40,8 @@ function toggleLike(likeIndex) {
 
 function addUsername(usernameIndex) {
   const usernameRef = document.getElementById(`name-area-${usernameIndex}`);
-  if (usernameRef.value !== "") {
+  const commentRef = document.getElementById(`comment-area-${usernameIndex}`);
+  if (usernameRef.value !== "" && commentRef.value !== "") {
     let newUsernameValue = usernameRef.value;
     usernameRef.value = "";
     books[usernameIndex].comments.push({ name: newUsernameValue });
@@ -48,6 +49,9 @@ function addUsername(usernameIndex) {
     const newUsernameHTML = returnUsernames(books[usernameIndex].comments.length - 1, usernameIndex);
     usernamesRef.innerHTML += newUsernameHTML;
     addComment(usernameIndex)
+  }
+  else{
+    alert('Bitte geben Sie Username & einen Kommentar ein.')
   }
 }
 
@@ -62,5 +66,13 @@ function addComment(commentIndex) { //das argument kommt aus der function return
     //in returncomments argumente aus addcomment(); books[commentIndex].comments.length - 1 ergibt die index der neuen comments länge -1 weil index start ist 0
     const newCommentHTML = returnComments(books[commentIndex].comments.length - 1, commentIndex); 
     commentsRef.innerHTML += newCommentHTML;
+/*     console.log(books[commentIndex].comments); // PROBLEM 
+ */    
   }
+}
+
+function fixPrice(i) {
+  const priceRef = document.getElementById(`showPrice-${i}`);
+  const price = priceRef.innerHTML.replace(",", "."); // fehler hat zeit gekostet! - nur das löschen von EUR im span hat nicht geholfen, vergessen das JS nicht mit , kann 
+  priceRef.innerHTML = parseFloat(price).toFixed(2);
 }
